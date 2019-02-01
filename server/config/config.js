425605c1cc845756d2aee72bd67a9d6889b190a0
ian maguiña <ianmaguina@gmail.com>
2019-02-01 17:11:53 -0500

    variable de entorno mongoDB

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:abc123@ds119085.mlab.com:19085/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
