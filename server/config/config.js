100ea58f485926eb1510f0bcb6bae946e3237c74
ian maguiña <ianmaguina@gmail.com>
2019-02-01 16:17:29 -0500

    rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,26 @@
-/*         
-        
-        puerto
-
- */
+//////////////////////////        
+//       puerto
+//////////////////////////
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//////////////////////////
+//      Entorno
+//////////////////////////
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//////////////////////////
+//   Base de Datos
+//////////////////////////
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:abc123@ds119085.mlab.com:19085/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
 const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,35 +12,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario');
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el nombre es necesario'
-        })
-
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario/:id', function(req, res) {
-    res.json('usuario borrado');
+mongoose.connect(process.env.URLDB, { useCreateIndex: true, useNewUrlParser: true }, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () => {
